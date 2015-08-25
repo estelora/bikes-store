d17c6bf8d532a9aa88737bfc4f5a280cfbe16718
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-25 19:12:13 -0400

    added testing files

--- test/conf.js
+++ test/conf.js
@@ -1,4 +1,5 @@
-/*conf.js for selenium test
+/* conf.js for selenium test
+ * run in terminal with: protractor conf.js
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['jsFile_spec.js']
---
+++ test/jsFile_spec.js
@@ -0,0 +1,21 @@
+describe('myFunction', function() {
+  it('should function', function() {
+    browser.get('https://angularjs.org');
+
+
+    /*
+    * ways to do stuff, check documentation on how to write the specific tests
+    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+    element(by.css('[value="add"]')).click();
+
+    var todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(3);
+    expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+    // You wrote your first test, cross it off the list
+    todoList.get(2).element(by.css('input')).click();
+    var completedAmount = element.all(by.css('.done-true'));
+    expect(completedAmount.count()).toEqual(2);
+    */
+  });
+});
